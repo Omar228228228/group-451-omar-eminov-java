8eaee5b4a58ac59603113a67ceb94183661eb4d4
Omar228228228 <a316972228@gmail.com>
2026-01-17 14:00:59 +0000

    classwork

---
+++ src/Classwork_3/Classwork_3.java
@@ -0,0 +1,11 @@
+package Classwork_3;
+
+import java.util.Arrays;
+
+public class Classwork_3 {
+    public static void main(String[] args) {
+        int[] jh = {1, 2, 3, 4, 5};
+        int[] ioi = Arrays.copyOf(jh, 7);
+        System.out.println(Arrays.toString(ioi));
+    }
+}
