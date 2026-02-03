0a89e2bcfb1ff5ab1a9800800c3bd45c66f636e9
Omar228228228 <a316972228@gmail.com>
2026-02-03 15:48:28 +0000

    Update BolnayaFantaziya.java

--- src/homework_2/BolnayaFantaziya.java
+++ src/homework_2/BolnayaFantaziya.java
@@ -1,4 +1,253 @@
 
 public class BolnayaFantaziya {
+	class GigaBorsh {
+	    void pokazatTemperaturu(int temperatura) {
+	        System.out.println(temperatura);
+	    }
+
+	    void ktoSelyo(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class TurboPelmeni {
+	    void skolkoSeli(int kolichestvo) {
+	        System.out.println(kolichestvo);
+	    }
+
+	    void komment(String text) {
+	        System.out.println(text);
+	    }
+	}
+
+	class UltraButer {
+	    void chtoVnutri(String sostav) {
+	        System.out.println(sostav);
+	    }
+
+	    void upalLi(int raz) {
+	        System.out.println(raz);
+	    }
+	}
+
+	class OptimusChay {
+	    void lozhkiSahara(int lozhki) {
+	        System.out.println(lozhki);
+	    }
+
+	    void ktoZavaril(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class GigaShashlik {
+	    void minutNaMangale(int minuti) {
+	        System.out.println(minuti);
+	    }
+
+	    void master(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class TurboSup {
+	    void temperatura(int t) {
+	        System.out.println(t);
+	    }
+
+	    void porciya(int p) {
+	        System.out.println(p);
+	    }
+	}
+
+	class UltraKasha {
+	    void gustota(int g) {
+	        System.out.println(g);
+	    }
+
+	    void tip(String t) {
+	        System.out.println(t);
+	    }
+	}
+
+	class OptimusPirog {
+	    void ves(int v) {
+	        System.out.println(v);
+	    }
+
+	    void nachinka(String n) {
+	        System.out.println(n);
+	    }
+	}
+
+	class GigaKotleta {
+	    void ves(int v) {
+	        System.out.println(v);
+	    }
+
+	    void ktoZharil(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class TurboSalat {
+	    void ingredienty(int i) {
+	        System.out.println(i);
+	    }
+
+	    void zapravka(String z) {
+	        System.out.println(z);
+	    }
+	}
+
+	class UltraMakaron {
+	    void minutiVariki(int m) {
+	        System.out.println(m);
+	    }
+
+	    void forma(String f) {
+	        System.out.println(f);
+	    }
+	}
+
+	class OptimusYayco {
+	    void kolichestvo(int k) {
+	        System.out.println(k);
+	    }
+
+	    void sposobGotovki(String s) {
+	        System.out.println(s);
+	    }
+	}
+
+	class GigaPizza {
+	    void kuski(int k) {
+	        System.out.println(k);
+	    }
+
+	    void ktoZakazal(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class TurboBurger {
+	    void kotleti(int k) {
+	        System.out.println(k);
+	    }
+
+	    void sostav(String s) {
+	        System.out.println(s);
+	    }
+	}
+
+	class UltraSendvich {
+	    void sloev(int s) {
+	        System.out.println(s);
+	    }
+
+	    void ktoSdelal(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class OptimusSyr {
+	    void lomti(int l) {
+	        System.out.println(l);
+	    }
+
+	    void vid(String v) {
+	        System.out.println(v);
+	    }
+	}
+
+	class GigaTort {
+	    void kuski(int k) {
+	        System.out.println(k);
+	    }
+
+	    void povod(String p) {
+	        System.out.println(p);
+	    }
+	}
+
+	class TurboBlini {
+	    void kolichestvo(int k) {
+	        System.out.println(k);
+	    }
+
+	    void ktoPek(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class UltraHolodec {
+	    void chasy(int c) {
+	        System.out.println(c);
+	    }
+
+	    void forma(String f) {
+	        System.out.println(f);
+	    }
+	}
+
+	class OptimusSosiska {
+	    void minutiZarki(int m) {
+	        System.out.println(m);
+	    }
+
+	    void ktoEl(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class GigaGrechka {
+	    void minutiVariki(int m) {
+	        System.out.println(m);
+	    }
+
+	    void ktoEl(String imya) {
+	        System.out.println(imya);
+	    }
+	}
+
+	class TurboRis {
+	    void porcii(int p) {
+	        System.out.println(p);
+	    }
+
+	    void sposob(String s) {
+	        System.out.println(s);
+	    }
+	}
+
+	class GigaNiga {
+	    void ves(int v) {
+	        System.out.println(v);
+	    }
+
+	    void vid(String t) {
+	        System.out.println(t);
+	    }
+	}
+
+	class OptimusKompot {
+	    void litri(int l) {
+	        System.out.println(l);
+	    }
+
+	    void frukti(String f) {
+	        System.out.println(f);
+	    }
+	}
+
+	class GigaZapekanka {
+	    void kuski(int k) {
+	        System.out.println(k);
+	    }
+
+	    void ktoDelal(String imya) {
+	        System.out.println(imya);
+	    }
+	}
 
 }
