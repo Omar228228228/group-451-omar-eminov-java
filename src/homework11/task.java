5446797d0ee09e0950a821e1fad9226619186f39
Omar228228228 <a316972228@gmail.com>
2026-03-31 15:49:16 +0100

    homework11

---
+++ src/homework11/task.java
@@ -0,0 +1,9 @@
+package homework11;
+
+public class task {
+    //1 blagodara GB programma mojet rabotat bistree potomu chto onop udalayet lishniye peremennniye.1 oop legche ponat.2 mojno ispolzovat gotoviy kod.3 mojno dobavlat noviye klassi bez izmeneniya koda.4danniye zashishenidostu.5 odin interface ispolzuyetsa dla raznix tipov obyektov 
+    //2 ???????????????????????????????????????????
+    //3 Date konkretniy moment vremeni
+    //4 java ispolzuyut v starix programmax. net yevo do six por ispolzuyut v bankax potomu chto on nadejniy. i v android prelojeniax
+    //5?
+}
