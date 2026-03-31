b73cb8299faf378bf46694c74ebe876bd60eba86
Omar228228228 <a316972228@gmail.com>
2026-03-31 16:19:48 +0100

    homework11

---
+++ src/homework11_2/task.java
@@ -0,0 +1,32 @@
+package homework11_2;
+
+public class task {
+    public static void main(String[] args) {
+        //1 
+        System.out.println("Baku,Guba,Shamaxi");
+        //2
+        String lg = "Java";
+        System.out.printf("Ya izuchayu %s\n", lg);
+        //3 YP eto kod blagodara kotoromu mojno napisat programmu
+        //toyest yp eto mama a programma eto sin
+
+
+        //4 sozdaniye fayla --> kampil proverayet sintaksis perevoda kod v bayt --> zapuskaniye koda
+        //5 v bankax potomuchto on nadejniy i ne ploxoy i v igrax
+        //6 analog:   java--mozgi programma chelovek java otvechsayert za deystviya programmi takje kak i mozgi u cheloveka
+        //7 obyasnite babushke
+
+        //2222222222222 zadaniye is pervovo punkta
+        //1?
+        //2  java1 1996 lts
+        //   java2 1998 ne lts
+        //   java5 2004 lts
+        //   java8 2014 lts
+        //   java9 2017 ne lts
+        //   java11 2018 lts
+        //   java17 2021 lts
+
+    
+
+    }
+}
